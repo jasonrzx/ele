57c5d2bb278826b933de7d98ddae7973282985a8
GHzhujindu <1069899428@qq.com>
2018-01-29 15:22:37 +0800

    status a shop.vue

---
+++ my-ele/src/components/shop.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="shop-box">
+		<div class="shop-top">
+			<div class="shop-top-T">
+				<router-link to="/">
+					<i class="icon iconfont">&#xe697;</i>
+				</router-link>
+			</div>
+		</div>
+		<div class="shop-center"></div>
+		<div class="shop-bottom"></div>
+	</div>
+</template>
+
+
+
+
+
+
+
+
+
+<script src="../assets/js/shop.js">
+	
+</script>
+
+<style  scpoed lang="scss" src="../assets/style/shop.scss">
+ @import '../assets/iconfont3/iconfonts.css'
+
+</style>
