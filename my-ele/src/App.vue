7e6ea9b47958aa97f592d3b97d89a94e4a9a23b5
jasonrzx <15604552120@163.com>
2018-01-29 21:10:16 +0800

    create dayone second

--- my-ele/src/App.vue
+++ my-ele/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,24 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './assets/iconfont/iconfont.css';
+html,body,div,nav,ul,li,ol,dl,dd,dt,p,h1,h2,h3,h4,h5,h6,form,fieldset,legend,img,input,textarea,figure,i,a,footer{margin:0;padding:0;}
+*{box-sizing:border-box;}
+h1,h2,h3,h4,h5,h6{font-size:16px;font-weight:100;}
+a,u{text-decoration:none;}
+ul,ol,li{list-style:none;}
+i,em{font-style:normal;}
+b,strong{font-weight:100;}
+img{
+  border:0;
+  display:block;
+}
+html,body{
+  width:100%;
+  height:100%;
+}
+body{
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ my-ele/src/components/MainHome.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="mainhome">
+		<div>
+			<MainHeader></MainHeader>
+		</div>
+		<div>
+			<MainFooter></MainFooter>
+		</div>
+	</div>
+</template>
+
+<script>
+import MainHeader from './MainHeader'
+import MainFooter from './MainFooter'
+export default {
+	name: "MainHome",
+	components: {
+		MainHeader,
+		MainFooter
+	}
+}
+</script>
+
+<style scoped>
+	
+</style>
